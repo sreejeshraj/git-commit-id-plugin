37582700ea11ce8752396894a55d4d037d1b0c88
admin <admin@WindowsLaptop>
2019-06-14 23:55:28 +0530

    1. Changed of annotations @Component and @Data 2. Added @formatter tag

--- src/main/java/com/ustglobal/demo/route/CamelDemoRoute.java
+++ src/main/java/com/ustglobal/demo/route/CamelDemoRoute.java
@@ -6,26 +6,31 @@ import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
-@Data
 @Component
-@ConfigurationProperties(prefix="camel-demo-route")
+@ConfigurationProperties(prefix = "camel-demo-route")
+@Data
+
 public class CamelDemoRoute extends RouteBuilder {
-	
+
 	private String routeId;
 
 	@Override
 	public void configure() throws Exception {
-		
+
+		// @formatter:off
+
 		from("file://{{inputFolder}}?delay=10s&noop=true")
 		.routeId("InputFolderToTestSedaRoute")
 		.setHeader("myHeader", constant("MY_HEADER_CONSTANT_VALUE"))
 		.to("seda://testSeda")
 		.log("**** Input File Pushed To Output Folder *****");
-		
+
 		from("seda://testSeda")
 		.routeId("TestSedaToOutputFolderRoute")
 		.to("file://{{outputFolder}}")
 		.log("***** myHeader: ${header.myHeader}");
+		
+		// @formatter:on
 
 	}
 
