d86980df50b9f887be4db3a1e6a018a5404e00a8
admin@WindowsLaptop <admin@WindowsLaptop>
2019-07-13 21:54:40 +0530

    Revert "Initial Commit"
    This reverts commit 4800740763d7a9b16e3b34029f663db80f7fa092.

--- src/main/java/com/ustglobal/demo/route/CamelDemoRoute.java
+++ src/main/java/com/ustglobal/demo/route/CamelDemoRoute.java
@@ -2,47 +2,46 @@ package com.ustglobal.demo.route;
 
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Component
-@ConfigurationProperties(prefix = "camel-demo-route")
+@ConfigurationProperties(prefix="camel-demo-route")
 @Data
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper=true)
 
 public class CamelDemoRoute extends RouteBuilder {
 
-	// The value of this property is injected from application.properties based on
-	// the profile chosen.
-	// private String injectedName;
-
-	private String query;
-	private String queryResultTopic;
-	private String queryInputTopic;
-	private String brokers;
-	private String timerFrequency;
+	// The value of this property is injected from application.properties based on the profile chosen.
+	private String injectedName;
 	
-	/*
-	 * @Autowired private JdbcTemplate jdbcTemplate;
-	 */
 	@Override
 	public void configure() throws Exception {
 
 		// @formatter:off
 		
-		// errorHandler(deadLetterChannel("seda:errorQueue").maximumRedeliveries(5).redeliveryDelay(1000));
+		errorHandler(deadLetterChannel("seda:errorQueue").maximumRedeliveries(5).redeliveryDelay(1000));
 
-				
-		from("timer://dbQueryTimer?period=10s")
-		.routeId("DATABASE_QUERY_TIMER_ROUTE")
-		.to("sql:SELECT version()?dataSource=#dataSource")	
-		//.to("sql:SELECT version()?dataSource=#mydatasource")
-		.log(LoggingLevel.INFO,"******Database query executed******");
+		from("file://{{inputFolder}}?delay=10s&noop=true")
+		.routeId("InputFolderToTestSedaRoute")
+		.setHeader("myHeader", constant("MY_HEADER_CONSTANT_VALUE"))
+		.to("seda://testSeda")
+		.log(LoggingLevel.DEBUG, "**** Input File Pushed To Output Folder ***** :"+injectedName);
+
+		from("seda://testSeda")
+		.routeId("TestSedaToOutputFolderRoute")
+		.to("file://{{outputFolder}}")
+		.log("***** myHeader: ${header.myHeader} ***** :"+injectedName);
+		
+		//Error Handling route!
+		
+		from("seda:errorQueue")
+		.routeId("ErrorHandlingRoute")
+		.log("***** error body: ${body} *****")
+		.log("***** Exception Caught: ${exception} *****");
 		
 		
 		// @formatter:on
